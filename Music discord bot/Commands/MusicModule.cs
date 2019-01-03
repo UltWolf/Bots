54a7e7c7f80fbb93ccbeb9052309685d742aaf70
UltWolf <ultwolf@gmail.com>
2019-01-03 12:13:05 +0200

    Add Music module and dll libraries for work with them

---
+++ Music discord bot/Commands/MusicModule.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_discord_bot.Commands
+{
+    public class MusicModule:ModuleBase
+    {
+        [Command("Log in ")]
+        public async Task logInChannel(IVoiceChannel channel = null)
+        {
+             
+            var voiceChannel  = await  Context.Client.GetChannelAsync(529744496188063744);
+            if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+ 
+            var audioClient = await channel.ConnectAsync();
+            await SendAsync(audioClient, "music/1.mp3");
+
+        }
+
+        [Command("Play")]
+        private async Task SendAsync(IAudioClient client, string path)
+        {
+            var ffmpeg = CreateStream(path);
+            var output = ffmpeg.StandardOutput.BaseStream;
+            var discord = client.CreatePCMStream(AudioApplication.Mixed);
+            await output.CopyToAsync(discord);
+            await discord.FlushAsync();
+        }
+        private Process CreateStream(string path)
+        {
+            var ffmpeg = new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-i {path} -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            };
+            return Process.Start(ffmpeg);
+        }
+    }
+}
---
+++ Music discord bot/Program.cs
@@ -0,0 +1,79 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Music_discord_bot
+{
+    class Program
+    {
+        private DiscordSocketClient client;
+        private IServiceProvider services;
+        public static void Main(string[] args)
+               => new Program().MainAsync().GetAwaiter().GetResult();
+        private CommandService command;
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+           
+            command = new CommandService()  ;
+      
+           
+
+            client.Log += Log;
+            string token = "";
+
+            string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Token.txt");
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    token = sr.ReadToEnd();
+                }
+
+            }
+
+            services = new ServiceCollection()
+              .BuildServiceProvider();
+            await InstallCommands();
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+       
+        public async Task InstallCommands()
+        {
+            client.MessageReceived += MessageReceived;
+            await command.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+
+        }
+
+        public async Task MessageReceived(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            int argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) return;
+
+            var context = new CommandContext(client, message);
+            var result = await command.ExecuteAsync(context, argPos, services);
+            if (!result.IsSuccess)
+            {
+                Console.WriteLine(DateTime.Now + " Error:" + result.ErrorReason);
+                await context.Channel.SendMessageAsync("We have some problems, please contact with admins.");
+            }
+        }
+
+    }
+}
